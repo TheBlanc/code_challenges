c6319eac14106f094cee58b62aca3b1ddd8614d1
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2019-09-09 13:12:07 -0400

    refactor solution to include 2d array. Still not format hackerrank is looking for :(

--- diagonalDiff.js
+++ diagonalDiff.js
@@ -1,25 +1,31 @@
 // Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
-var matrix = [ 4,
-               16, 24, 5, -3,
-               12, 32, -20, 5,
-               7, 8, 40, -3,
-               21, 32, -15, 13
-               ]
+var matrix1 = [ 4,
+               [16, 24, 5, -3],
+               [12, 32, -20, 5],
+               [7, 8, 40, -3],
+               [21, 32, -15, 13]
+              ]
+
+var matrix2 = [ 3,
+                [11, 2, 4],
+                [4, 5, 6],
+                [10, 8, -12],
+              ]
 
 function diagonalDifference(arr){
   var leftDiagSum = 0;
   var rightDiagSum = 0;
-  var diagDiff = 0;
+  var x = 0;
+  var y = arr[1].length - 1;
 
-  for (var i = 1; i < arr.length; i+= (arr[0] + 1)) {
-    leftDiagSum += arr[i];
+  for (var i = 1; i < arr.length; i++) {
+    leftDiagSum += arr[i][x];
+    rightDiagSum += arr[i][y];
+    x++;
+    y--;
   }
-  for (var i = arr[0]; i < arr.length - (arr[0] - 1); i+= (arr[0] -1)) {
-    rightDiagSum += arr[i];
-  }
-
-  diagDiff = Math.abs(leftDiagSum - rightDiagSum);
+  var diagDiff = Math.abs(leftDiagSum - rightDiagSum);
   return diagDiff;
 }
-console.log(diagonalDifference(matrix));
+console.log(diagonalDifference(matrix2));
